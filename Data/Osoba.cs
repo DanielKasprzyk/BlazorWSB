4d40f9073df2e605840a0815b7f5a78c474af679
Daniel Kasprzyk <dkwarmaster3@gmail.com>
2020-12-06 15:57:15 +0100

    Add project files.

---
+++ Data/Osoba.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prezenty.Data
+{
+    public class Osoba
+    {
+
+        public string Nazwiso { get; set; }
+        public string Imie { get; set; }
+        public int Wiek { get; set; }
+        public string PrezentOd { get; set; }
+        public bool Zaslozyl { get; set; }
+    }
+}
---
+++ Data/OsobaService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prezenty.Data
+{
+    public class OsobaService
+    {
+        private static List<Osoba> Osoby = new List<Osoba>();
+
+        public OsobaService()
+        {
+            Osoby.Add(new Osoba() { 
+                Imie ="Daniel",
+                Nazwiso ="Kasprzyk",
+                Wiek = 24,
+                PrezentOd = "Mikołaj",
+                Zaslozyl = false
+            });
+
+            Osoby.Add(new Osoba()
+            {
+                Imie = "Elvior",
+                Nazwiso = "RavenClan",
+                Wiek = 25,
+                PrezentOd = "Sigurd",
+                Zaslozyl = false
+            });
+
+            Osoby.Add(new Osoba()
+            {
+                Imie = "Jhon",
+                Nazwiso = "Snow",
+                Wiek = 20,
+                PrezentOd = "Daenerys",
+                Zaslozyl = true
+            });
+        }
+
+        public static Task<List<Osoba>> PobierzLsteOsobAsync()
+        {
+            return Task.FromResult(Osoby);
+        }
+    }
+}
